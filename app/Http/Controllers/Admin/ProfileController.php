2ad5c056bae8e017e55f1c721302fa77259aeeba
EC2 Default User <ec2-user@ip-172-31-10-246.us-east-2.compute.internal>
2020-04-05 08:31:13 +0000

    chapter16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -25,18 +25,51 @@ class ProfileController extends Controller
         unset($form['_token']);
         
         $profile->fill($form);
-        $news->save();
+        $profile->save();
     
         return redirect('admin/profile/create');
     }
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+    //public function edit()
+    //{
+        //return view('admin.profile.edit');
+    //}
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+    //public function update()
+    //{
+        //return redirect('admin/profile/edit');
+    //}
+    
+     public function edit(Request $request)
+  {
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+    
+    public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
+  }
+  
+  public function delete(Request $request)
+  {
+      // 該当するProfile Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  } 
 }
